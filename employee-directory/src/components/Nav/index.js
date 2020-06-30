55ece02488b3d6385ba7b15b6f3c748d89e34ebf
Jason Meli <jdmmeli@yahoo.com>
2020-06-30 18:49:06 -0400

    initial commit

---
+++ employee-directory/src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Nav() {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">Employee Directory</nav>
+    )
+}
+
+export default Nav
